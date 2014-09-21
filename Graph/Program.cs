6650368bd1faef33875d1c22896e1971ace52d0c
Bhavesh Dave <bhavesh.dave@bizmobile.co.jp>
2014-09-21 19:36:54 +0900

    added
    added

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Graph
 {
@@ -27,6 +28,43 @@ namespace Graph
 			Console.WriteLine ();
 			Console.WriteLine ("Vertex Edges Connected to");
 			g.ShowEdgesOfVertex (v1);
+
+
+			Graph gr = new Graph (10);
+
+			gr.AddVertex ("A");
+			gr.AddVertex ("B");
+			gr.AddVertex ("C");
+			gr.AddVertex ("D");
+			gr.AddVertex ("E");
+			gr.AddVertex ("F");
+			gr.AddVertex ("G");
+			gr.AddVertex ("H");
+			gr.AddVertex ("I");
+			gr.AddVertex ("J");
+
+			Vertex m1 = gr.GetVertexAtIndex (0);
+			Vertex m2 = gr.GetVertexAtIndex (1);
+			Vertex m3 = gr.GetVertexAtIndex (2);
+			Vertex m4 = gr.GetVertexAtIndex (3);
+			Vertex m5 = gr.GetVertexAtIndex (4);
+			Vertex m6 = gr.GetVertexAtIndex (5);
+			Vertex m7 = gr.GetVertexAtIndex (6);
+			Vertex m8 = gr.GetVertexAtIndex (7);
+			Vertex m9 = gr.GetVertexAtIndex (8);
+			Vertex m10 = gr.GetVertexAtIndex (9);
+
+			gr.AddEdge (m1, m2);
+			gr.AddEdge (m2, m3);
+			gr.AddEdge (m3, m4);
+			gr.AddEdge (m1, m5);
+			gr.AddEdge (m5, m6);
+			gr.AddEdge (m6, m7);
+			gr.AddEdge (m1, m8);
+			gr.AddEdge (m8, m9);
+			gr.AddEdge (m9, m10);
+			gr.DepthFirstSearch ();
+			gr.BredthFirstSearch ();
 
 			Console.ReadLine ();
 		}
@@ -108,6 +146,64 @@ namespace Graph
 			for (int i = 0; i < Edges.GetLength(0); i++) {
 				if (Edges [thisIndex, i] == 1)
 					Console.Write (GetVertexAtIndex (i).label + " ,");
+			}
+		}
+
+		private int GetAdjUnvisitedVertex(int v)
+		{
+			for (int i = 0; i < Vertices.Length; i++) {
+				if ((Edges[v,i] == 1) && (Vertices[i].wasVisited == false)) {
+					//Console.WriteLine (" Adj. vertex visited? " + Vertices [i].wasVisited.ToString ());
+					return i;
+
+				}
+
+			}
+			return -1;
+		}
+
+		public void DepthFirstSearch()
+		{
+			Stack stck = new Stack ();
+			Vertices [0].wasVisited = true;
+			Console.WriteLine ();
+			Console.WriteLine ( " Depth First Search Result ");
+			Console.WriteLine (Vertices [0].label);
+			stck.Push(0);
+			while (stck.Count >0) {
+				int v = GetAdjUnvisitedVertex (Convert.ToInt32(stck.Peek ()));
+				if (v == -1)
+					stck.Pop ();
+				else {
+					Vertices [v].wasVisited = true;
+					Console.WriteLine (Vertices [v].label + " " + Vertices[v].wasVisited.ToString());
+					stck.Push (v);
+				}
+			}
+
+			for (int i = 0; i < Vertices.Length; i++) {
+				GetVertexAtIndex (i).wasVisited = false;
+			}
+
+		}
+
+		public void BredthFirstSearch()
+		{
+			Queue que = new Queue ();
+			Vertices [0].wasVisited = true;
+			Console.WriteLine (" Bredth First Search Result ");
+			Console.WriteLine (Vertices [0].label);
+			que.Enqueue (0);
+			while (que.Count > 0) {
+				int ver1 = (int)que.Dequeue ();
+				int ver2 = GetAdjUnvisitedVertex (ver1);
+				while (ver2 != -1) {
+					Vertices [ver2].wasVisited = true;
+					Console.WriteLine (Vertices [ver2].label);
+					que.Enqueue (ver2);
+					ver2 = GetAdjUnvisitedVertex (ver1);
+				}
+
 			}
 		}
 
---
+++ HeapSort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+
+namespace HeapSort
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Heap h = new Heap (5);
+			h.insert (1);
+			h.insert (3);
+			h.insert (2);
+			h.insert (5);
+			h.insert (4);
+			Console.WriteLine ("Sorted Elements are :");
+			for (int i = 0; i < h.Length(); i++) {
+				Console.WriteLine (h.GetValueAtIndex (i).ToString ());
+			}
+			Console.ReadLine ();
+		}
+
+
+
+
+	}
+
+	class Heap
+	{
+		int[] arr;
+		int current = 0;
+		public Heap(int length)
+		{
+			arr = new int[length];
+		}
+
+		public int Length()
+		{
+			return arr.Length;
+		}
+
+		public int GetValueAtIndex(int x)
+		{
+			return arr [x];
+		}
+
+		public void insert(int x)
+		{
+			arr [current] = x;
+			Swim (arr, current);
+			current++;
+		}
+
+		public void Swim(int[] arr, int k)
+		{
+			while ((k >= 1) && (arr[k/2] < arr[k])) {
+
+				int temp = arr [k / 2];
+				arr [k / 2] = arr [k];
+				arr [k] = temp;
+				k = k / 2;
+
+			}
+		}
+	}
+}
